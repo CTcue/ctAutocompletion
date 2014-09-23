cadce934fadc822d230f44d9701e8c3f177b186b
Jochem de Boer <jochem.deboer@outlook.com>
2014-09-23 22:54:16 +0200

    adapted config for production

--- config/config.js
+++ config/config.js
@@ -1,12 +1,24 @@
-var config = {
-  "host"  : "localhost",
-  "port"  : 4050,
-  "path"  : "http://localhost:4050/"
-};
+var config = {}
+
+config.host = process.env.NODE_ENV === 'production'
+              ? '188.226.214.70'
+              : 'localhost';
+
+config.port = process.env.NODE_ENV === 'production'
+              ? 80
+              : 4050;
+
+config.path = 'http://'
+              + config.host
+              + ':'
+              + config.port;
+
 
 config.elastic = {
   db : {
-    "host"  : "http://" + config.host + ":9200"
+    "host"  : "http://"
+              + config.host
+              + ":9200"
   }
 };
 
