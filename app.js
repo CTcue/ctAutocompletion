b082dfa50c2d7e7d52559577352ca73873e83e52
Jochem de Boer <jochem.deboer@outlook.com>
2014-09-23 22:22:39 +0200

    Working

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/** Module dependencies. */
+var config        = require('./config/config');
+var koa           = require('koa');
+var app           = module.exports = koa();
+var _             = require('lodash');
+
+/** KOA Middleware */
+var cors          = require('koa-cors');
+var router        = require('koa-trail');
+var parse         = require('koa-parse-json');
+var json          = require('koa-json');
+var helmet        = require('koa-helmet');
+
+var autocomplete  = require('./controllers/autocompleteController.js');
+var elasticQuery = require('./lib/elasticQuery.js');
+
+// JSON output
+app.use(json({ pretty: true, param: 'pretty' ***REMOVED***));
+
+// Security (XSS)
+app.use(helmet.defaults());
+
+// Needed for authentication/authorization
+app.use(cors({
+  'headers' : [ 'Content-Type', 'Authorization' ]
+***REMOVED***));
+
+// Body parser
+app.use(parse());
+
+// Routing
+app.use(router(app));
+
+/** API */
+app.all('/', function *() {
+  this.body = {
+    success : true
+  ***REMOVED***;
+***REMOVED***);
+
+app.post('/autocomplete', function *() {
+  var searchQuery = this.request.body.query;
+
+  var results = yield autocomplete(searchQuery);
+
+  this.body = results;
+
+***REMOVED***);
+
+// Listen
+app.listen(config.port);
+console.log('listening on port %d', config.port);
---
+++ controllers/autocompleteController.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/** Module dependencies. */
+var config    = require('../config/config.js');
+var _         = require('lodash');
+var reqClient = require('request-json').newClient(config.elastic.db.host);
+
+function autocomplete(searchQuery) {
+  return function(callback) {
+
+    var results = [];
+
+    var lookup = {
+      "result" : {
+        "text" : searchQuery,
+        "completion" : {
+          "size"  : 150,
+          "field" : "title",
+          "fuzzy" : {
+            "min_length"    : 3,
+            "prefix_length" : 5
+      ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***
+***REMOVED***;
+
+    reqClient.post('/diagnose/_suggest', lookup,
+      function (err, response, body) {
+
+        if (err && err.code === "ECONNREFUSED") {
+          console.log("Please start elasticsearch!");
+          process.exit(1);
+    ***REMOVED***;
+
+        if (body
+          && body.result
+          && body.result[0].options
+          && body.result[0].options.length > 0) {
+
+          results = body.result[0].options;
+
+      ***REMOVED*** Create usable object
+          results = _.map(results, function(result) {
+            return result.text.split(' | ')[0];
+      ***REMOVED***);
+
+      ***REMOVED*** Sort shortest to longest
+          results = results.sort(function(a, b) {
+            return a.length - b.length;
+      ***REMOVED***);
+
+    ***REMOVED***;
+
+        callback(err, results);
+  ***REMOVED***);
+
+  ***REMOVED***;
+***REMOVED***;
+
+module.exports = autocomplete;
