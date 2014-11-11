69e92d1f722edd912e7d0b3d3a45ddf6ac543844
Jochem de Boer <jochem.deboer@outlook.com>
2014-11-11 16:57:34 +0100

    New, cheaper memory upload script for autocomplete

--- scripts/populateAutocomplete.js
+++ scripts/populateAutocomplete.js
@@ -36,12 +36,44 @@ var elasticClient = new elastic.Client(elasticOptions);
 * Global Variables
 */
 
-  var mapping = {
-    "settings"  : acConfig.indexSettings,
-    "mappings"  : {***REMOVED***
-  ***REMOVED***;
+var mapping = {
+  "settings"  : acConfig.indexSettings,
+  "mappings"  : {***REMOVED***
+***REMOVED***;
 
-var storage = {***REMOVED***;
+var umlsCountQuery = [
+  "SELECT COUNT(*)",
+  "FROM MRSTY a, MRCONSO b",
+  "WHERE a.STY IN ('"
+  + acConfig.semanticTypes.join("', '") + "')",
+  "AND a.CUI = b.CUI"
+].join(' ');
+
+var umlsRetrieveQuery = [
+  "SELECT b.CUI, b.AUI, a.STY, b.STR",
+  "FROM MRSTY a, MRCONSO b",
+  "WHERE a.STY IN ('"
+  + acConfig.semanticTypes.join("', '") + "')",
+  "AND a.CUI = b.CUI LIMIT 40000"
+].join(' ');
+
+var transformBar = new ProgressBar(' [:bar] :percent :etas Transforming atoms to documents', {
+          complete: '*'
+        , incomplete: '.'
+        , width: 50
+        , total: 100
+***REMOVED***);
+
+var transformChunk = 0;
+
+var uploadBar = new ProgressBar(' [:bar] :percent :etas Uploading documents', {
+          complete: '*'
+        , incomplete: '.'
+        , width: 50
+        , total: 100
+***REMOVED***);
+
+var uploadChunk = 0;
 
 /*
 * Functions
@@ -53,7 +85,7 @@ function deleteIndex(index, callback) {
   reqClient.del(index, function (err, response, body) {
     if (body.error
       && body.error === 'IndexMissingException[[' + index + '] missing]') {
-      console.log('\n ' + index + ' does not exist\n');
+      console.log('\n Index\'' + index + '\' does not exist');
       callback(null, 'success');
 ***REMOVED*** else if (body.error
       && body.error !== 'IndexMissingException[[' + index + '] missing]') {
@@ -92,151 +124,40 @@ function mapIndex(index, callback) {
   ***REMOVED***);
 ***REMOVED***;
 
-// Initialize a storage variable for the semantic types and their arrays
+// Count atoms in MYSQL UMLS database
 
-function insertSemanticTypes(semanticType, callback) {
-  storage[semanticType] = {
-    'name'  : semanticType,
-  ***REMOVED***;
-  callback();
-***REMOVED***;
-
-function initializeStorage(semanticTypes, callback) {
-  async.each(semanticTypes, insertSemanticTypes, function (err) {
+function countAtoms(callback) {
+  mysql.query(umlsCountQuery, function (err, counts) {
     if (err) {
       callback(err);
 ***REMOVED*** ***REMOVED***
-      console.log(' Storage Initialized');
-      callback(null, 'success');
+      console.log(' Atom Count: ' + counts[0]['COUNT(*)']);
+      callback(null, counts[0]['COUNT(*)']);
 ***REMOVED***;
   ***REMOVED***);
 ***REMOVED***;
 
-// MYSQL UMLS query for counting and retrieving atoms per semantic type
+// Retrieve atoms from MYSQL UMLS database
 
-function umlsQuery(semanticType, count, callback) {
-  var query = [
-      "FROM MRSTY a, MRCONSO b",
-      "WHERE a.STY = '" + semanticType + "'",
-      "AND a.CUI = b.CUI"
-    ];
-
-    if (count === true) {
-      query.unshift("SELECT COUNT(*)");
-***REMOVED*** ***REMOVED***
-      query.unshift("SELECT b.CUI, b.AUI, a.STY, b.STR");
-***REMOVED***;
-
-  callback(query.join(" "));
-***REMOVED***;
-
-// Initialize atomCountQuery per semantic type
-
-function createAtomCountQuery(semanticType, callback) {
-  umlsQuery(semanticType, true, function (query) {
-    storage[semanticType].atomCountQuery = query;
-    callback();
-  ***REMOVED***);
-***REMOVED***;
-
-function initializeAtomCountQueries(semanticTypes, callback) {
-  async.each(semanticTypes, createAtomCountQuery, function (err) {
+function retrieveAtoms(atomCount, callback) {
+  console.log(' Retrieving all atoms. This can take up to 5 minutes...\n');
+  mysql.query(umlsRetrieveQuery, function (err, records) {
     if (err) {
       callback(err);
 ***REMOVED*** ***REMOVED***
-      console.log(' Atom Count Queries initialized');
-      callback(null, 'success');
+      transformChunk  = 100 / records.length;
+      uploadChunk     = 100 / records.length;
+      console.log(' Atoms retrieved: ' + records.length);
+      console.log(' All atoms accounted for: '
+                  + (atomCount === records.length)
+                  + '\n');
+      console.log(' Uploading all atoms');
+      callback(null, records);
 ***REMOVED***;
   ***REMOVED***);
 ***REMOVED***;
 
-// Count the amount of atoms per semantic type in the MYSQL UMLS database
-// and insert into storage variable
-
-function retrieveAtomCount(semanticType, callback) {
-    mysql.query(storage[semanticType].atomCountQuery, function (err, records) {
-    if (err) {
-      callback(err);
-***REMOVED*** ***REMOVED***
-      storage[semanticType].atomCount = records[0]['COUNT(*)'];
-      callback();
-***REMOVED***;
-  ***REMOVED***);
-***REMOVED***;
-
-function countAtoms(semanticTypes, callback) {
-  console.log(' Counting atoms per semantic types... ');
-  async.each(semanticTypes, retrieveAtomCount, function (err) {
-    if (err) {
-      callback(err);
-***REMOVED*** ***REMOVED***
-      console.log(' Done');
-      callback(null, 'success');
-***REMOVED***;
-  ***REMOVED***);
-***REMOVED***;
-
-// Initialize atomRetrieveQuery per semantic type
-
-function createAtomRetrieveQuery(semanticType, callback) {
-  umlsQuery(semanticType, false, function (query) {
-    storage[semanticType].atomRetrieveQuery = query;
-    callback();
-  ***REMOVED***);
-***REMOVED***;
-
-function initializeAtomRetrieveQueries(semanticTypes, callback) {
-  async.each(semanticTypes, createAtomRetrieveQuery, function (err) {
-    if (err) {
-      callback(err);
-***REMOVED*** ***REMOVED***
-      console.log(' Atom Retrieve Queries initialized\n');
-      callback(null, 'success');
-***REMOVED***;
-  ***REMOVED***);
-***REMOVED***;
-
-// Retrieve atoms per semantic type from the MYSQL UMLS database
-// and insert into storage variable
-
-function retrieveAtoms(semanticType, callback) {
-
-  console.log('   Retrieving '
-    + storage[semanticType].atomCount
-    + ' atoms from semantic type '
-    + semanticType + '...');
-
-  mysql.query(storage[semanticType].atomRetrieveQuery, function (err, records) {
-    if (err) {
-      callback(err);
-***REMOVED*** ***REMOVED***
-      storage[semanticType].atoms = records;
-      console.log('   Atoms retrieved: '
-        + storage[semanticType].atoms.length
-        + ' - Al atoms accounted for: '
-        + (storage[semanticType].atomCount === storage[semanticType].atoms.length)
-        + '\n');
-      callback();
-***REMOVED***;
-  ***REMOVED***);
-***REMOVED***;
-
-function storeAtoms(semanticTypes, callback) {
-
-  console.log(' Retrieving atoms\n');
-
-  async.eachSeries(semanticTypes, retrieveAtoms, function (err) {
-    if (err) {
-      callback(err);
-***REMOVED*** ***REMOVED***
-      console.log(' Atoms Retrieved and stored\n');
-      callback(null, 'success');
-***REMOVED***;
-  ***REMOVED***);
-***REMOVED***;
-
-// Transform stored atoms per semantic type into document format
-// for ElasticSearch
+// Transform MYSQL atoms into JSON atoms
 
 function removeStopWords(word, callback) {
 
@@ -250,6 +171,20 @@ function removeStopWords(word, callback) {
 
 ***REMOVED***;
 
+function uploadDocument(document, callback) {
+
+  elasticClient.index(document, function (error, response) {
+    uploadBar.tick(uploadChunk);
+    if (error) {
+      console.error(error);
+      setImmediate(callback);
+***REMOVED*** ***REMOVED***
+      setImmediate(callback);
+***REMOVED***;
+  ***REMOVED***);
+
+***REMOVED***;
+
 function transformAtom(atom, callback) {
 
   async.filterSeries(atom.STR.split(' '), removeStopWords, function (results) {
@@ -257,133 +192,67 @@ function transformAtom(atom, callback) {
   ***REMOVED***);
 
   var document = {
-      "index" : acConfig.index,
-      "type"  : atom.STY.toLowerCase().replace(/\s/g, '_'),
-      "body"  : {
-        "atom"  : {
-          "input"   : atom.input,
-          "output"  : atom.STR,
-          "payload" : {
-            "cui"           : atom.CUI,
-            "aui"           : atom.AUI,
-            "semanticType"  : atom.STY
-      ***REMOVED***
+    "index" : acConfig.index,
+    "type"  : atom.STY.toLowerCase().replace(/\s/g, '_'),
+    "body"  : {
+      "atom"  : {
+        "input"   : atom.input,
+        "output"  : atom.STR.toLowerCase()
+             .replace(/'/g, "")
+             .replace(/\W+/g, " ")
+             .replace(/\s+/g, " ")
+             .trim(),
+        "payload" : {
+          "cui"           : atom.CUI,
+          "aui"           : atom.AUI,
+          "semanticType"  : atom.STY
     ***REMOVED***
   ***REMOVED***
-***REMOVED***;
-
-  storage[atom.STY].transformedAtoms.push(document);
-
-  setImmediate(callback);
-***REMOVED***;
-
-function transformAtoms(semanticTypeIndex, callback) {
-  var semanticType = acConfig.semanticTypes[semanticTypeIndex]
-
-  storage[semanticType].transformedAtoms = [];
-
-  console.log('   Transforming '
-  + storage[semanticType].atomCount
-  + ' atoms from semantic type '
-  + semanticType + '...');
-
-  async.eachSeries(storage[semanticType].atoms, transformAtom, function (err) {
-    if (err) {
-      callback(err)
-***REMOVED*** ***REMOVED***
-      delete storage[semanticType].atoms;
-      console.log('   Atoms transformed: '
-      + storage[semanticType].transformedAtoms.length
-      + ' - Al atoms accounted for: '
-      + (storage[semanticType].atomCount === storage[semanticType].transformedAtoms.length)
-      + '\n');
-      callback();
-***REMOVED***;
-  ***REMOVED***);
-***REMOVED***;
-
-function transformAllAtoms(semanticTypes, callback) {
-
-  console.log(' Transforming atoms\n');
-
-  async.timesSeries(semanticTypes.length, transformAtoms, function (err) {
-    if (err) {
-      callback(err)
-***REMOVED*** ***REMOVED***
-      console.log(' All atoms transformed to document format\n');
-      callback(null, 'success');
-***REMOVED***;
-  ***REMOVED***);
-***REMOVED***;
-
-// Upload the transformed atoms per semantic type to ElasticSearch
-
-function uploadAtoms(semanticTypeIndex, callback) {
-  var semanticType = acConfig.semanticTypes[semanticTypeIndex]
-
-  console.log('   Uploading '
-  + storage[semanticType].transformedAtoms.length
-  + ' atoms from semantic type '
-  + semanticType + '...');
-
-  var uploadBar = new ProgressBar('   [:bar] :percent :etas', {
-            complete: '*'
-          , incomplete: '.'
-          , width: 20
-          , total: 100
-  ***REMOVED***);
-
-  var progressChunk = 100 / storage[semanticType].transformedAtoms.length;
-
-  async.eachSeries(storage[semanticType].transformedAtoms
-  , function uploadAtom(atom, callback) {
-
-    elasticClient.index(atom, function (error, response) {
-      uploadBar.tick(progressChunk);
-      if (error) {
-        console.error('elasticsearch cluster is down!');
-        setImmediate(callback);
-  ***REMOVED*** ***REMOVED***
-        setImmediate(callback);
+***REMOVED***
   ***REMOVED***;
-***REMOVED***);
 
-  ***REMOVED***, function (err) {
+  document.body.atom.input.push(atom.STR.toLowerCase()
+             .replace(/'/g, "")
+             .replace(/\W+/g, " ")
+             .replace(/\s+/g, " ")
+             .trim());
+
+  // transformBar.tick(transformChunk);
+
+  uploadDocument(document, function () {
+    callback();
+  ***REMOVED***);
+***REMOVED***;
+
+// Execute count, retrieve, transform and upload atoms
+
+function retrieveTransformUploadAtoms(semanticTypes, callback) {
+  countAtoms(function (err, atomCount) {
     if (err) {
-      callback(err)
+      callback(err);
 ***REMOVED*** ***REMOVED***
-      console.log('   Success\n');
-      callback();
+
+      retrieveAtoms(atomCount, function (err, records) {
+        if (err) {
+          callback(err);
+    ***REMOVED*** ***REMOVED***
+
+          async.eachSeries(records, transformAtom, function (err) {
+            if (err) {
+              callback(err);
+        ***REMOVED*** ***REMOVED***
+
+              callback();
+
+        ***REMOVED***;
+      ***REMOVED***);
+
+    ***REMOVED***;
+  ***REMOVED***);
 ***REMOVED***;
   ***REMOVED***);
 ***REMOVED***;
 
-function uploadAllAtoms(semanticTypes, callback) {
-  console.log(' Uploading atoms\n');
-
-  async.timesSeries(semanticTypes.length, uploadAtoms, function (err) {
-    if (err) {
-      callback(err)
-***REMOVED*** ***REMOVED***
-      console.log(' All atoms uploaded to ElasticSearch\n');
-      callback(null, 'success');
-***REMOVED***;
-  ***REMOVED***);
-***REMOVED***;
-
-// Close the MYSQL connection
-
-function closeMYSQL(callback) {
-  mysql.end();
-  callback(null, 'success');
-***REMOVED***;
-
-// Close the ElasticSearch connection
-
-function closeElasticSearch(callback) {
-  elasticClient.close();
-  callback(null, 'success');
-***REMOVED***;
 
 /*
 * Execution
@@ -391,118 +260,40 @@ function closeElasticSearch(callback) {
 
 async.series({
 
-  deleteIndex : function (nextFunction) {
+  deleteAndMapIndex : function (nextFunction) {
     deleteIndex(acConfig.index, function (err, success) {
       if (err) {
         nextFunction(err);
   ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
+        mapIndex(acConfig.index, function (err, success) {
+          if (err) {
+            nextFunction(err);
+      ***REMOVED*** ***REMOVED***
+            nextFunction(null, success);
+      ***REMOVED***;
+    ***REMOVED***);
   ***REMOVED***;
 ***REMOVED***);
   ***REMOVED***,
 
-  mapIndex : function (nextFunction) {
-    mapIndex(acConfig.index, function (err, success) {
+  retrieveTransformUploadAtoms : function (nextFunction) {
+    retrieveTransformUploadAtoms(acConfig.semanticTypes, function (err, success) {
       if (err) {
         nextFunction(err);
   ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
+        nextFunction(null, 'success');
   ***REMOVED***;
 ***REMOVED***);
   ***REMOVED***,
 
-  initializeStorage : function (nextFunction) {
-    initializeStorage(acConfig.semanticTypes, function (err, success) {
-      if (err) {
-        nextFunction(err);
-  ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
-  ***REMOVED***;
-***REMOVED***);
+  endMYSQL : function (nextFunction) {
+    mysql.end();
+    nextFunction(null, 'success');
   ***REMOVED***,
 
-  initializeAtomCountQueries : function (nextFunction) {
-    initializeAtomCountQueries(acConfig.semanticTypes, function (err, success) {
-      if (err) {
-        nextFunction(err);
-  ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
-  ***REMOVED***;
-***REMOVED***);
-  ***REMOVED***,
-
-  countAtoms : function (nextFunction) {
-    countAtoms(acConfig.semanticTypes, function (err, success) {
-      if (err) {
-        nextFunction(err);
-  ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
-  ***REMOVED***;
-***REMOVED***);
-  ***REMOVED***,
-
-  initializeAtomRetrieveQueries : function (nextFunction) {
-    initializeAtomRetrieveQueries(acConfig.semanticTypes, function (err, success) {
-      if (err) {
-        nextFunction(err);
-  ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
-  ***REMOVED***;
-***REMOVED***);
-  ***REMOVED***,
-
-  storeAtoms : function (nextFunction) {
-    storeAtoms(acConfig.semanticTypes, function (err, success) {
-      if (err) {
-        nextFunction(err);
-  ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
-  ***REMOVED***;
-***REMOVED***);
-  ***REMOVED***,
-
-  transformAllAtoms : function (nextFunction) {
-    transformAllAtoms(acConfig.semanticTypes, function (err, success) {
-      if (err) {
-        nextFunction(err);
-  ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
-  ***REMOVED***;
-***REMOVED***);
-  ***REMOVED***,
-
-  uploadAllAtoms : function (nextFunction) {
-    uploadAllAtoms(acConfig.semanticTypes, function (err, success) {
-      if (err) {
-        nextFunction(err);
-  ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
-  ***REMOVED***;
-***REMOVED***)
-  ***REMOVED***,
-
-  closeMYSQL : function (nextFunction) {
-    closeMYSQL(function (err, success) {
-      if (err) {
-        nextFunction(err);
-  ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
-  ***REMOVED***;
-***REMOVED***);
-  ***REMOVED***,
-
-  closeElasticSearch : function (nextFunction) {
-    closeElasticSearch(function (err, success) {
-      if (err) {
-        nextFunction(err);
-  ***REMOVED*** ***REMOVED***
-        nextFunction(null, success);
-  ***REMOVED***;
-***REMOVED***);
-  ***REMOVED***,
-
-  Afsluiting : function (nextFunction) {
-      nextFunction(null, 'success');
+  endElastic : function (nextFunction) {
+    elasticClient.close();
+    nextFunction(null, 'success');
   ***REMOVED***
 
 ***REMOVED***, function (err, results) {
