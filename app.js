bafdac09b6c8274ab85618a40c57d810ea636dc2
Fabien Tesselaar <fabientesselaar@gmail.com>
2014-11-26 18:51:57 +0100

    Updated the uploading of UMLS items and autocomplete function revamp

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 'use strict';
 
 /** Module dependencies. */
-var config        = require('./config/config');
-var koa           = require('koa');
-var app           = module.exports = koa();
-var _             = require('lodash');
+var config = require('./config/config');
+var koa    = require('koa');
+var app    = koa();
 
 /** KOA Middleware */
-var cors          = require('koa-cors');
-var router        = require('koa-trail');
-var parse         = require('koa-parse-json');
-var json          = require('koa-json');
-var helmet        = require('koa-helmet');
+var cors   = require('koa-cors');
+var router = require('koa-trail');
+var json   = require('koa-json');
+var helmet = require('koa-helmet');
+var parse  = require('./lib/koa-request-body');
 
-var autocomplete  = require('./controllers/autocompleteController.js');
-var autocomplete_v2  = require('./controllers/autocompleteController_v2.js');
-// var elasticQuery  = require('./lib/elasticQuery.js');
+var autocomplete = require('./controllers/autocomplete.js');
+var sugar        = require('sugar');
+var _            = require('lodash');
 
 // JSON output
 app.use(json({ pretty: true, param: 'pretty' ***REMOVED***));
@@ -34,29 +33,148 @@ app.use(parse());
 // Routing
 app.use(router(app));
 
+
 /** API */
+
 app.all('/', function *() {
   this.body = {
     success : true
   ***REMOVED***;
 ***REMOVED***);
 
-app.post('/autocomplete', function *() {
-  var searchQuery = this.request.body.query;
+app.post('/autocomplete', 
+  checkBody, 
+  simpleQuery, 
+  recordSet
+);
 
-  var suggests    = yield autocomplete(searchQuery);
+app.post('/autocomplete/diagnosis', 
+  checkBody, 
+  diagnosisQuery, 
+  recordSet
+);
 
-  this.body       = suggests;
+app.post('/autocomplete/medicine', 
+  checkBody, 
+  medicineQuery, 
+  recordSet
+);
 
-***REMOVED***);
+// Takes an Elasticsearch result and returns the list { CUI : Text ***REMOVED***. 
+function * recordSet(next) {
+  var set = this.body.suggestions.suggest[0], tmp = [];
 
-app.post('/autocomplete_v2', function *() {
-  var searchQuery = this.request.body.query;
+  // TODO check additional suggestions
+  if (set.options.length == 0) {
+    return this.body = [];
+  ***REMOVED***
 
-  var suggests    = yield autocomplete_v2(searchQuery);
+  // Get the payloads
+  set = _.pluck(set.options, 'payload');
+  
+  // For each entry, get the CUI+STR that most matches the query
+  for (var i in set) {
+    tmp[i] = {
+      '_id' : set[i].cui
+***REMOVED***;
 
-  this.body       = suggests;
-***REMOVED***)
+    var filtered = _.filter(set[i].codes, function(str) {
+      return str.startsWith(this);
+***REMOVED***, this.body.query);
+
+    if (filtered.length == 1) {
+      tmp[i].str = _.sortBy(filtered, 'length')[0];
+***REMOVED***
+    ***REMOVED***
+      filtered = _.filter(set[i].codes, function(str) {
+        return str.startsWith(this);
+  ***REMOVED***, this.body.query.slice(0, (this.body.query.length+2)*0.5));
+
+      if (filtered.length > 0) {
+        tmp[i].str = _.sortBy(filtered, 'length')[0];
+  ***REMOVED***
+      ***REMOVED***
+        tmp[i].str = set[i].codes[0];  
+  ***REMOVED***
+***REMOVED***
+  ***REMOVED***
+
+  // Generate 5 bins
+  tmp = _.groupBy(tmp, function(a) {
+    var N = a.str.length - this.length;
+
+    if      (N <= 8)  return 0;
+    else if (N <= 13) return 1;
+    else if (N <= 18) return 2;
+    else if (N <= 27) return 3;
+    else              return 4;
+
+  ***REMOVED***, this.body.query);
+
+
+  // For the bins, put terms that start with the query first 
+  // and skip entries in the last bin
+  tmp = _.reduce(tmp, function(result, v, k) {
+    var list = sortByStartsWith(v, this);
+
+    if (typeof result == 'undefined')
+      return list;
+    else 
+      return result.concat(list);
+
+  ***REMOVED***, [], this.body.query);
+
+  this.body = tmp.slice(0,12);
+***REMOVED***;
+
+// Put this in LIB functions
+function sortByStartsWith(list, term) { 
+  if (list.length <= 1)
+    return list;
+
+  list = _.groupBy(list, function(obj) {
+    return obj.str.startsWith(this);
+  ***REMOVED***, term);
+
+  if (!!list['true'] && !!list['false'])
+    return list['true'].concat(list['false']);
+  else if (!!list['true'])
+    return list['true'];
+  else
+    return list['false'];
+***REMOVED***
+
+function * diagnosisQuery(next) {
+  this.body.suggestions = yield autocomplete.diagnosis(this.body.query);
+
+  yield next;
+***REMOVED***
+
+function * medicineQuery(next) {
+  this.body.suggestions = yield autocomplete.medicine(this.body.query);
+
+  yield next;
+***REMOVED***
+
+function * simpleQuery(next) {
+  this.body.suggestions = yield autocomplete.simple(this.body.query);
+
+  yield next;
+***REMOVED***
+
+function * checkBody(next) {
+  if (!this.request.body || _.isEmpty(this.request.body) || !!!this.request.body.query) {
+    return this.body = {
+      'error' : true,
+      'msg'   : 'Please provide a valid JSON request body.'
+***REMOVED***
+  ***REMOVED***
+
+  this.body = {***REMOVED***;
+  this.body.query = this.request.body.query;
+
+  yield next;
+***REMOVED***;
 
 // Listen
 app.listen(config.port);
---
+++ controllers/autocomplete.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/** Module dependencies. */
+var config    = require('../config/config.js');
+var _         = require('lodash');
+var elastic   = require('elasticsearch');
+
+var elasticClient = new elastic.Client({
+  host : config.elastic
+***REMOVED***);
+
+var lookup =  {
+  "index" : "autocomplete",
+  "body" : {
+    "suggest" : {
+      "text" : "",
+      "completion" : {
+        "field" : "complete",
+        "size"  : 80,
+        "fuzzy" : true,
+
+        "context" : { 
+          "type" : []
+    ***REMOVED***
+  ***REMOVED***
+***REMOVED***
+  ***REMOVED***
+***REMOVED***;
+
+
+exports.simple = function(query) {
+  return function(callback) {
+***REMOVED*** TODO If query has multiple words ...
+    
+    lookup.body.suggest.text = query;
+    lookup.body.suggest.completion.context.type = [
+      "disease_or_syndrome",
+      "sign_or_symptom",
+      "neoplastic_process"
+    ];
+
+    elasticClient.suggest(lookup, function (err, res) {
+      callback(err, res);
+***REMOVED***);
+  ***REMOVED***;
+***REMOVED***;
+
+
+exports.diagnosis = function(query) {
+  return function(callback) {
+***REMOVED*** TODO If query has multiple words ...
+    
+    lookup.body.suggest.text = query;
+    lookup.body.suggest.completion.context.type = [
+      "disease_or_syndrome",
+      "sign_or_symptom",
+      "pathologic_function",
+      "mental_or_behavioral_dysfunction",
+      "cell_or_molecular_dysfunction",
+      "injury_or_poisoning",
+      "neoplastic_process", 
+      "experimental_model_of_disease"
+    ];
+
+    elasticClient.suggest(lookup, function (err, res) {
+      callback(err, res);
+***REMOVED***);
+  ***REMOVED***;
+***REMOVED***;
+
+
+exports.medicine = function(query) {
+  return function(callback) {
+***REMOVED*** TODO If query has multiple words ...
+    
+    lookup.body.suggest.text = query;
+    lookup.body.suggest.completion.context.type = [
+      "clinical_drug"
+    ];
+
+    elasticClient.suggest(lookup, function (err, res) {
+      callback(err, res);
+***REMOVED***);
+  ***REMOVED***;
+***REMOVED***;
+
