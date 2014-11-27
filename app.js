cfeac4dd4fcfb9a8f30896df0045646f992d1fcd
Fabien Tesselaar <fabientesselaar@gmail.com>
2014-11-27 17:14:07 +0100

    Fixed scoring and fuzzy minimum length

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ var json   = require('koa-json');
 var helmet = require('koa-helmet');
 var parse  = require('./lib/koa-request-body');
 
-var autocomplete = require('./controllers/autocomplete.js');
 var sugar        = require('sugar');
 var _            = require('lodash');
+
+var autocomplete = require('./controllers/autocomplete.js');
+var filters      = require('./lib/filters.js');
 
 // JSON output
 app.use(json({ pretty: true, param: 'pretty' ***REMOVED***));
@@ -44,25 +46,24 @@ app.all('/', function *() {
 
 app.post('/autocomplete', 
   checkBody, 
-  simpleQuery, 
-  recordSet
+  simpleQuery
 );
 
 app.post('/autocomplete/diagnosis', 
   checkBody, 
-  diagnosisQuery, 
-  recordSet
+  diagnosisQuery 
 );
 
 app.post('/autocomplete/medicine', 
   checkBody, 
-  medicineQuery, 
-  recordSet
+  medicineQuery
 );
 
-// Takes an Elasticsearch result and returns the list { CUI : Text ***REMOVED***. 
-function * recordSet(next) {
-  var set = this.body.suggestions.suggest[0], tmp = [];
+
+
+function * diagnosisQuery(next) {
+  var suggestions = yield autocomplete.diagnosis(this.body.query);
+  var set = suggestions.suggest[0];
 
   // TODO check additional suggestions
   if (set.options.length == 0) {
@@ -71,95 +72,47 @@ function * recordSet(next) {
 
   // Get the payloads
   set = _.pluck(set.options, 'payload');
-  
-  // For each entry, get the CUI+STR that most matches the query
-  for (var i in set) {
-    tmp[i] = {
-      '_id' : set[i].cui
-***REMOVED***;
 
-    var filtered = _.filter(set[i].codes, function(str) {
-      return str.startsWith(this);
-***REMOVED***, this.body.query);
+  // Get recommendations
+  set = filters.suggestionsFromElasticRecords(set, this.body.query);
 
-    if (filtered.length == 1) {
-      tmp[i].str = _.sortBy(filtered, 'length')[0];
-***REMOVED***
-    ***REMOVED***
-      filtered = _.filter(set[i].codes, function(str) {
-        return str.startsWith(this);
-  ***REMOVED***, this.body.query.slice(0, (this.body.query.length+2)*0.5));
-
-      if (filtered.length > 0) {
-        tmp[i].str = _.sortBy(filtered, 'length')[0];
-  ***REMOVED***
-      ***REMOVED***
-        tmp[i].str = set[i].codes[0];  
-  ***REMOVED***
-***REMOVED***
-  ***REMOVED***
-
-  // Generate 5 bins
-  tmp = _.groupBy(tmp, function(a) {
-    var N = a.str.length - this.length;
-
-    if      (N <= 8)  return 0;
-    else if (N <= 13) return 1;
-    else if (N <= 18) return 2;
-    else if (N <= 27) return 3;
-    else              return 4;
-
-  ***REMOVED***, this.body.query);
-
-
-  // For the bins, put terms that start with the query first 
-  // and skip entries in the last bin
-  tmp = _.reduce(tmp, function(result, v, k) {
-    var list = sortByStartsWith(v, this);
-
-    if (typeof result == 'undefined')
-      return list;
-    else 
-      return result.concat(list);
-
-  ***REMOVED***, [], this.body.query);
-
-  this.body = tmp.slice(0,12);
-***REMOVED***;
-
-// Put this in LIB functions
-function sortByStartsWith(list, term) { 
-  if (list.length <= 1)
-    return list;
-
-  list = _.groupBy(list, function(obj) {
-    return obj.str.startsWith(this);
-  ***REMOVED***, term);
-
-  if (!!list['true'] && !!list['false'])
-    return list['true'].concat(list['false']);
-  else if (!!list['true'])
-    return list['true'];
-  else
-    return list['false'];
-***REMOVED***
-
-function * diagnosisQuery(next) {
-  this.body.suggestions = yield autocomplete.diagnosis(this.body.query);
-
-  yield next;
+  this.body = set.slice(0,12); 
 ***REMOVED***
 
 function * medicineQuery(next) {
-  this.body.suggestions = yield autocomplete.medicine(this.body.query);
+  var suggestions = yield autocomplete.medicine(this.body.query);
+  var set = suggestions.suggest[0];
 
-  yield next;
+  // TODO check additional suggestions
+  if (set.options.length == 0) {
+    return this.body = [];
+  ***REMOVED***
+
+  // Get the payloads
+  set = _.pluck(set.options, 'payload');
+
+  // Get recommendations
+  set = filters.suggestionsFromElasticRecords(set, this.body.query);
+
+  this.body = set.slice(0,12); 
 ***REMOVED***
 
 function * simpleQuery(next) {
-  this.body.suggestions = yield autocomplete.simple(this.body.query);
+  var suggestions = yield autocomplete.simple(this.body.query);
+  var set = suggestions.suggest[0];
 
-  yield next;
+  // TODO check additional suggestions
+  if (set.options.length == 0) {
+    return this.body = [];
+  ***REMOVED***
+
+  // Get the payloads
+  set = _.pluck(set.options, 'payload');
+
+  // Get recommendations
+  set = filters.suggestionsFromElasticRecords(set, this.body.query);
+
+  this.body = set.slice(0,12); 
 ***REMOVED***
 
 function * checkBody(next) {
@@ -169,6 +122,12 @@ function * checkBody(next) {
       'msg'   : 'Please provide a valid JSON request body.'
 ***REMOVED***
   ***REMOVED***
+
+  // Queries should not be empty or single character
+  if (this.request.body.query.length <= 1) {
+    return this.body = [];
+  ***REMOVED***
+  
 
   this.body = {***REMOVED***;
   this.body.query = this.request.body.query;
