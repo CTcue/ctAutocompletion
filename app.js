250b7065dcef3a77ab8310b8928b0fc7deefa23a
Fabien Tesselaar <fabientesselaar@gmail.com>
2014-11-30 17:35:04 +0100

    Added suggestions if multiple words fail to look into single, non stopwords, words

--- app.js
+++ app.js
@@ -44,76 +44,11 @@ app.all('/', function *() {
   ***REMOVED***;
 ***REMOVED***);
 
-app.post('/autocomplete', 
-  checkBody, 
-  simpleQuery
+app.post('/autocomplete/:type', 
+  checkBody,
+  medQuery 
 );
 
-app.post('/autocomplete/diagnosis', 
-  checkBody, 
-  diagnosisQuery 
-);
-
-app.post('/autocomplete/medicine', 
-  checkBody, 
-  medicineQuery
-);
-
-
-
-function * diagnosisQuery(next) {
-  var suggestions = yield autocomplete.diagnosis(this.body.query);
-  var set = suggestions.suggest[0];
-
-  // TODO check additional suggestions
-  if (set.options.length == 0) {
-    return this.body = [];
-  ***REMOVED***
-
-  // Get the payloads
-  set = _.pluck(set.options, 'payload');
-
-  // Get recommendations
-  set = filters.suggestionsFromElasticRecords(set, this.body.query);
-
-  this.body = set.slice(0,12); 
-***REMOVED***
-
-function * medicineQuery(next) {
-  var suggestions = yield autocomplete.medicine(this.body.query);
-  var set = suggestions.suggest[0];
-
-  // TODO check additional suggestions
-  if (set.options.length == 0) {
-    return this.body = [];
-  ***REMOVED***
-
-  // Get the payloads
-  set = _.pluck(set.options, 'payload');
-
-  // Get recommendations
-  set = filters.suggestionsFromElasticRecords(set, this.body.query);
-
-  this.body = set.slice(0,12); 
-***REMOVED***
-
-function * simpleQuery(next) {
-  var suggestions = yield autocomplete.simple(this.body.query);
-  var set = suggestions.suggest[0];
-
-  // TODO check additional suggestions
-  if (set.options.length == 0) {
-    return this.body = [];
-  ***REMOVED***
-
-  // Get the payloads
-  set = _.pluck(set.options, 'payload');
-
-  // Get recommendations
-  set = filters.suggestionsFromElasticRecords(set, this.body.query);
-
-  this.body = set.slice(0,12); 
-***REMOVED***
 
 function * checkBody(next) {
   if (!this.request.body || _.isEmpty(this.request.body) || !!!this.request.body.query) {
@@ -128,12 +63,86 @@ function * checkBody(next) {
     return this.body = [];
   ***REMOVED***
   
-
   this.body = {***REMOVED***;
-  this.body.query = this.request.body.query;
+  this.body.query = this.request.body.query.trim().toLowerCase();
 
   yield next;
 ***REMOVED***;
+
+
+function * medQuery(next) {
+  var suggestions = yield autocomplete.fn(this.body.query, this.params.type);
+  var set = suggestions.suggest[0];
+
+  // If we have suggestions for given query
+  if (set.options.length > 0) {
+***REMOVED*** Get the payloads + best recommendations
+    set = _.pluck(set.options, 'payload');
+    set = filters.suggestionsFromElasticRecords(set, this.body.query);
+
+    return this.body = set.slice(0, 12); 
+  ***REMOVED***
+  ***REMOVED***
+***REMOVED*** No suggestions yet
+    var words = this.body.query.words();
+
+***REMOVED*** Does the query contain multiple words?
+    if (words.length > 0) {
+      var payloads = {***REMOVED***;
+      var cuiCodes = [];
+
+  ***REMOVED*** For the additional words
+      for (var i=0, L=words.length; i<L; i++) {
+        var suggestions = yield autocomplete.fn(words[i], this.params.type, querySize(words[i]), "words");
+        var set = suggestions.suggest[0];
+            set = _.pluck(set.options, 'payload');
+
+        payloads = _.assign(set);
+        cuiCodes.push( _.pluck(set, 'cui'));
+  ***REMOVED***
+
+      if (words.length == 2) {
+        var intersection = _.intersection(cuiCodes[0], cuiCodes[1]);
+  ***REMOVED***
+      else if (words.length == 3) {
+        var intersection = _.intersection(cuiCodes[0], cuiCodes[1], cuiCodes[2]);
+  ***REMOVED***
+      ***REMOVED***
+        var intersection = _.intersection(cuiCodes[0], cuiCodes[1], cuiCodes[2], cuiCodes[3]);
+  ***REMOVED***
+
+  ***REMOVED*** TODO If intersection is empty, skip a word?
+
+      var set = _.filter(payloads, function(a) {
+        return this.indexOf(a.cui) >= 0;
+  ***REMOVED***, intersection);
+
+  ***REMOVED*** Get recommendations
+      set = filters.suggestionsFromElasticRecords(set, words[0]);
+
+      return this.body = set.slice(0, 12);
+***REMOVED***
+  ***REMOVED***
+
+  // TODO look for larger set?
+
+  this.body = [];
+***REMOVED***
+
+function querySize(str) {
+  var N = str.length;
+
+  if (N <= 5) {
+    return 150;
+  ***REMOVED***
+  else if (N < 10) {
+    return 100;
+  ***REMOVED***
+  ***REMOVED***
+    return 80;
+  ***REMOVED***
+***REMOVED***
+
 
 // Listen
 app.listen(config.port);
