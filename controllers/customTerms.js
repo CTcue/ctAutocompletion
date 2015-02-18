05711760a73995420195bd9e7d710026b7b79aeb
Fabien Tesselaar <fabientesselaar@gmail.com>
2015-02-18 13:13:47 +0100

    Adjusted populate script

---
+++ controllers/customTerms.js
@@ -0,0 +1,9 @@
+
+var config = require('../config/config.js');
+var client = require('../lib/requestClient.js');
+
+module.exports = function *() {
+
+  //var result = yield client.post(path, lookup);
+  this.body = {};
+};
