b98afaf5df233369358592540ff452fc63c4a00c
Fabien Tesselaar <fabientesselaar@gmail.com>
2015-04-27 22:01:07 +0200

    Updated expander

--- controllers/expandQuery.js
+++ controllers/expandQuery.js
@@ -1,25 +1,32 @@
 
 var config = require('../config/config.js');
-var client = require('../lib/requestClient.js');
+var elastic = require('elasticsearch');
+var elasticClient = new elastic.Client({
+    "apiVersion" : "1.4"
+***REMOVED***);
 
 module.exports = function *() {
-  var path = config.elastic + "/expander/records/_search?size=1";
-
-  var lookup = {
-    "_source" : ["str"],
-    "query": {
-      "match": {
-        "cui": this.body.query
-  ***REMOVED***
-***REMOVED***
+  var result = yield function(callback) {
+      elasticClient.search({
+          "index" : 'expander',
+          "type"  : 'records',
+          "body" : {
+              "query" : {
+                  "term" : {
+                      "cui" : this.body.query
+               ***REMOVED***
+           ***REMOVED***
+      ***REMOVED***
+  ***REMOVED***,
+      function(err, resp) {
+          if (resp && !!resp.hits && resp.hits.total > 0) {
+            callback(false, resp.hits.hits[0]._source.str);
+      ***REMOVED***
+          ***REMOVED***
+            callback(err, []);
+      ***REMOVED***
+  ***REMOVED***);
   ***REMOVED***;
 
-  var result = yield client.post(path, lookup);
-
-  if (result.hits.total >= 1) {
-    this.body = result.hits.hits[0]._source.str;
-  ***REMOVED***
-  ***REMOVED***
-    this.body = [];
-  ***REMOVED***
+  this.body = result;
 ***REMOVED***;
