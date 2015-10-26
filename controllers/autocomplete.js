a0628632f6f42ed2843549da9a93980e95c386d4
Fabiën <fabientesselaar@gmail.com>
2015-10-26 17:07:39 +0100

    Added es suggestion for single term completion

--- controllers/autocomplete.js
+++ controllers/autocomplete.js
@@ -8,81 +8,128 @@ var elasticClient = new elastic.Client({
 ***REMOVED***);
 
 module.exports = function *() {
-  var response = {
-    "took"  : 220,
-    "hits"  : []
-  ***REMOVED***;
 
   var query = this.request.body.query;
   var selectedIds = this.request.body.selectedIds || [];
 
-  var result = yield findTerms(query, selectedIds);
-  if (!result || result.hits.total === 0) {
-      result = yield findFuzzyTerms(query, selectedIds);
-  ***REMOVED***
+  var split = query.split(" ")
+  var response = {***REMOVED***;
 
-  if (result && result.hits.total > 0) {
-      response.took = result.took;
-      var resultHits = result.hits.hits;
-
-      for (var i=0, N=resultHits.length; i<N; i++) {
-        response.hits[i]       = resultHits[i]._source;
-        response.hits[i].score = resultHits[i]._score;
+  if (split.length <= 1) {
+      response = yield findSingle(query, selectedIds);
   ***REMOVED***
+  ***REMOVED***
+      response = yield findTerms(query, selectedIds);
   ***REMOVED***
 
   this.body = response;
 ***REMOVED***;
 
+function findSingle(query, selectedIds) {
+    return function(callback) {
+        var elastic_query =  {
+            "term-suggest": {
+                "text": query.trim(),
+                "completion": {
+                    "field": "suggest",
+                    "size": 10,
+                     "fuzzy" : {
+                       "prefix_length": 5,
+                       "fuzziness" : 0.6
+                ***REMOVED***
+            ***REMOVED***
+        ***REMOVED***
+    ***REMOVED***;
+
+        var queryObj = {
+            "index" : 'autocomplete',
+            "type"  : 'records',
+            "body"  : elastic_query
+    ***REMOVED***;
+
+        elasticClient.suggest(queryObj, function(err, res) {
+            var hits = res['term-suggest'][0]["options"];
+            var result = [];
+
+            for (var i=0; i<hits.length; i++) {
+                result.push({
+                  "cui": hits[i].payload['cui'],
+                  "str": hits[i].text
+            ***REMOVED***);
+        ***REMOVED***
+
+            callback(err, {
+                "took": 120,
+                "hits": result
+        ***REMOVED***);
+    ***REMOVED***);
+***REMOVED***
+***REMOVED***
 
 function findTerms(query, selectedIds) {
 ***REMOVED*** Filter out CUI codes that the user already selected
     return function(callback) {
-        elasticClient.search({
-            "index" : 'autocomplete',
-            "type"  : 'records',
-            "body" : {
+        var elastic_query =  {
+            "_source": ["cui", "str", "source"],
 
-              "query" : {
-                    "filtered" : {
-                        "query" : {
-                            "match_phrase" : {
-                              "str" :  query.trim()
+            "query": {
+                "filtered" : {
+                    "query" : {
+                        "function_score": {
+                            "query" : {
+                                "match_phrase" : {
+                                    "str" : query.trim()
+                            ***REMOVED***
+                        ***REMOVED***,
+                            "functions": [
+                               {
+                                 "filter": {
+                                    "term": { "types": ["disorder", "diagnosis", "medication", "Disease or Syndrome", "Disease/Finding"] ***REMOVED***
+                              ***REMOVED***,
+                                 "weight": 2
+                           ***REMOVED***,
+                               {
+                                  "field_value_factor" : {
+                                      "field": "votes",
+                                      "modifier": "log1p",
+                                      "factor":   2
+                              ***REMOVED***
+                           ***REMOVED***
+                            ]
+                    ***REMOVED***
+                ***REMOVED***,
+
+                    "filter" : {
+                        "not" : {
+                            "terms" : {
+                                "cui" : selectedIds
                         ***REMOVED***
-                    ***REMOVED***,
-
-                        "filter" : {
-                          "not" : {
-                              "terms" : {
-                                  "cui" : selectedIds
-                          ***REMOVED***
-                      ***REMOVED***
                     ***REMOVED***
                 ***REMOVED***
             ***REMOVED***
         ***REMOVED***
-    ***REMOVED***, function(err, res) { callback(err, res); ***REMOVED***);
-***REMOVED***
-***REMOVED***
+    ***REMOVED***;
 
-
-function findFuzzyTerms(query, selectedIds) {
-***REMOVED*** Filter out CUI codes that the user already selected
-    return function(callback) {
-        elasticClient.search({
+        var queryObj = {
             "index" : 'autocomplete',
             "type"  : 'records',
-            "body" : {
-                "query" : {
-                    "match" : {
-                        "str" : {
-                          "type": "phrase",
-                          "fuzziness": "AUTO",
-                          "query" :  query.trim()
-                    ***REMOVED***
-                ***REMOVED***
+            "body"  : elastic_query
+    ***REMOVED***;
+
+        elasticClient.search(queryObj, function(err, res) {
+            var hits = res.hits;
+            var result = [];
+
+            if (hits.total > 0) {
+                for (var i=0; i<hits.hits.length; i++) {
+                    result.push(hits.hits[i]._source);
             ***REMOVED***
         ***REMOVED***
-    ***REMOVED***, function(err, res) { callback(err, res); ***REMOVED***);
+
+            callback(err, {
+                "took": res.took,
+                "hits": result
+        ***REMOVED***);
+    ***REMOVED***);
 ***REMOVED***
 ***REMOVED***
