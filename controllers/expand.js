4518104eae5f415506b01d559c5c83e99d21d976
Fabiën <fabientesselaar@gmail.com>
2015-11-06 13:44:08 +0100

    Changed expand to include type

--- controllers/expand.js
+++ controllers/expand.js
@@ -19,9 +19,7 @@ module.exports = function *() {
       },
       function(err, resp) {
           if (resp && !!resp.hits && resp.hits.total > 0) {
-            callback(false, resp.hits.hits.map(function(item) {
-                return item._source.str;
-            }));
+            callback(false, resp.hits.hits);
           }
           else {
             callback(err, []);
@@ -29,5 +27,35 @@ module.exports = function *() {
       });
   };
 
-  this.body = result;
+  var types = result[0]._source.types;
+
+  this.body = {
+    "type": "-",
+    "category": typeToCategory(types),
+    "terms": result.map(function(item) { return item._source.str; })
+  };
 };
+
+
+function typeToCategory(types) {
+    if (!types || typeof types === "undefined" || types.length === 0) {
+        return "diagnosis";
+    }
+
+    var medication = ["medication", "Pharmacologic Substance", "product"]
+    var labresult  = ["Laboratory Procedure", "Chemical/Ingredient"]
+
+    return inList(types, medication, "medication") ||
+           inList(types, labresult, "labresult") ||
+           "diagnosis";
+}
+
+function inList(haystack, search, type) {
+    for (var i=0; i<search.length; i++) {
+          if (haystack.indexOf(search[i]) >= 0) {
+              return type;
+          }
+    }
+
+    return false;
+}
