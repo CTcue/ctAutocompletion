c8297671d23dc0261cd5864c41978f2fa09731db
Fabiën <fabientesselaar@gmail.com>
2016-01-20 17:25:51 +0100

    Updated query to get related terms in one go

--- controllers/suggest.js
+++ controllers/suggest.js
@@ -1,6 +1,7 @@
 
 var config  = require('../config/config.js');
 var neo4j = require('neo4j');
+var _ = require("lodash");
 
 var db = new neo4j.GraphDatabase({
     url: 'http://localhost:7474',
@@ -9,7 +10,8 @@ var db = new neo4j.GraphDatabase({
 
 
 module.exports = function *() {
-    var query = 'MATCH (t1:Concept { term:{A***REMOVED*** ***REMOVED***),(t2:Concept { term:{B***REMOVED*** ***REMOVED***), p = allShortestPaths((t1)-[:is_part_of*..2]-(t2)) return p';
+***REMOVED*** var query = 'MATCH (t1:Concept { name:{A***REMOVED*** ***REMOVED***), (t2:Concept { name:{B***REMOVED*** ***REMOVED***), p = allShortestPaths((t1)-[:is_part_of*..2]-(t2)) return p, ID(p)';
+    var query = 'MATCH (t1:Concept { name:{A***REMOVED*** ***REMOVED***), (t2:Concept { name:{B***REMOVED*** ***REMOVED***), (g:Group), (t1)-[:is_part_of]->(g)<-[:is_part_of]-(t2), g<-[:is_part_of]-(r) return t1, t2, g, ID(g) as ID, COLLECT(r) as terms'
     var params = this.request.body.query;
 
     var cypherObj = {
@@ -23,18 +25,36 @@ module.exports = function *() {
 ***REMOVED***;
 
     var result = yield function(callback) {
-        db.cypher(cypherObj, function(err, result) {
+        db.cypher(cypherObj, function(err, paths) {
             if (err) {
-          ***REMOVED*** console.log(err);
+              console.log(err);
               callback(false, [])
         ***REMOVED***
 
-        ***REMOVED*** console.log(result)
-            callback(null, result)
+            var found = [];
+
+            for (var i=0; i < paths.length; i++) {
+                var finding = paths[i];
+
+            ***REMOVED*** If no group found, skip
+                if (! finding.hasOwnProperty("g")) {
+                    continue;
+            ***REMOVED***
+
+                var rel = {
+                    "groupId": finding["ID"],
+                    "groupName": finding["g"]["name"],
+                    "abbreviation": finding["g"]["abbr"] || "",
+                    "description" : finding["g"]["description"] || "",
+                    "groupTerms"  : finding["terms"]
+            ***REMOVED***
+
+                found.push(rel);
+        ***REMOVED***
+
+            callback(null, found)
     ***REMOVED***);
 ***REMOVED***
 
-    this.body = {
-        "related": result
-***REMOVED***
+    this.body = result;
 ***REMOVED***;
