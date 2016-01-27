639d1a2b0a72d558cd4da4a3d6ee27c33e8843b3
Fabiën <fabientesselaar@gmail.com>
2016-01-27 15:04:07 +0100

    Allow mix of CUI/str so custom term can still be suggested

--- controllers/suggest.js
+++ controllers/suggest.js
@@ -11,39 +11,12 @@ var db = new neo4j.GraphDatabase({
 
 module.exports = function *() {
     var params = this.request.body.query;
-    var cypherObj = false;
 
-    // Query can be a list of terms or list of object with cui + string
-    if (typeof params[0] === "string") {
-        var query = 'MATCH (t1:Concept { name:{A} }), (t2:Concept { name:{B} }), (g:Group), (t1)-[:is_part_of]->(g)<-[:is_part_of]-(t2), g<-[:is_part_of]-(r) return t1, t2, g, ID(g) as ID, COLLECT(r) as terms'
-
-        var cypherObj = {
-            "query"  : query,
-            "params" : {
-                "A": params[0].toLowerCase(),
-                "B": params[1].toLowerCase()
-            },
-
-            "lean": true
-        };
-    }
-    else if (typeof params[0] === "object" && params[0].hasOwnProperty("cui")) {
-        var query = 'MATCH (t1:Concept { cui:{A} }), (t2:Concept { cui:{B} }), (g:Group), (t1)-[:is_part_of]->(g)<-[:is_part_of]-(t2), g<-[:is_part_of]-(r) return t1, t2, g, ID(g) as ID, COLLECT(r) as terms'
-
-        var cypherObj = {
-            "query"  : query,
-            "params" : {
-                "A": params[0].cui || "",
-                "B": params[1].cui || ""
-            },
-
-            "lean": true
-        };
-    }
-    else {
+    if (params.length < 2) {
         return this.body = [];
     }
 
+    var cypherObj = buildCypherObj(params);
 
     var result = yield function(callback) {
         db.cypher(cypherObj, function(err, paths) {
@@ -79,3 +52,49 @@ module.exports = function *() {
 
     this.body = result;
 };
+
+
+function buildCypherObj(params) {
+    var cypherObj = false;
+
+    var A = checkParam(params[0]);
+    var B = checkParam(params[1]);
+
+    if (A && B) {
+        cypherObj = {
+            "query"  : buildQuery(A["type"], B["type"]),
+            "params" : {
+                "A": A["str"],
+                "B": B["str"]
+            },
+
+            "lean": true
+        };
+    }
+
+    return cypherObj;
+}
+
+function checkParam(param) {
+    if (typeof param === "string") {
+        return { "type": "name", "str": param };
+    }
+
+    if (typeof param === "object") {
+        if (param.hasOwnProperty("cui") && param.cui.length > 1) {
+            return { "type": "cui", "str": param.cui };
+        }
+
+        return { "type": "name", "str": param.str || param.name || "" };
+    }
+
+    return false;
+}
+
+function buildQuery(keyA, keyB) {
+    return `MATCH (t1:Concept { ${keyA}: {A} }), (t2:Concept { ${keyB}: {B} }),
+            (g:Group),
+            (t1)-[:is_part_of]->(g)<-[:is_part_of]-(t2),
+            g<-[:is_part_of]-(r)
+            return t1, t2, g, ID(g) as ID, COLLECT(r) as terms`
+}
