7a8f5d2251699dfddeaa24f374779da9b5f2cd67
Fabiën <fabientesselaar@gmail.com>
2016-02-09 14:31:57 +0100

    Added fuzziness for term_lookup

--- controllers/term_lookup.js
+++ controllers/term_lookup.js
@@ -12,16 +12,14 @@ var elasticClient = new elastic.Client({
 });
 
 
-const source = ["cui", "str", "exact", "pref", "source", "types"];
+const source = ["cui", "str", "exact", "pref"];
 
 
 module.exports = function *() {
   var query = this.request.body.query;
 
-
   // Lookup matches in Elasticsearch
   var exactMatches = yield findExact(query);
-  // var closeMatches = yield findMatches(query);
 
   this.body = {
     "took": exactMatches.took,
@@ -41,91 +39,16 @@ function findExact(query) {
             "size": 3,
 
             "query": {
-                "term" : {
-                    "exact" : wantedTerm
+                "fuzzy" : {
+                    "exact" : {
+                        "value": wantedTerm,
+                        "fuzziness": (wantedTerm.length > 5 ? 2 : 0)
+                    }
                 }
             }
         };
 
         // Search in all indexes
-        var queryObj = {
-            "index" : 'autocomplete',
-            "body"  : elastic_query
-        };
-
-        elasticClient.search(queryObj, function(err, res) {
-            var hits = res.hits;
-            var result = [];
-
-            if (hits && hits.total > 0) {
-                for (var i=0; i < hits.hits.length; i++) {
-                    result.push(hits.hits[i]._source);
-                }
-            }
-
-            callback(err, {
-                "took": res.took,
-                "hits": result
-            });
-        });
-    }
-}
-
-
-function findMatches(query) {
-    var origin = guess_origin(query);
-
-    // Filter out CUI codes that the user already selected
-    return function(callback) {
-
-        // DBC code check needs prefix matching
-        if (origin === "code") {
-            var elastic_query =  {
-                "_source": source,
-                "size": 6,
-
-                "query": {
-                    "match_phrase_prefix" : {
-                        "str" : query.trim()
-                    }
-                }
-            };
-        }
-        else {
-            var elastic_query =  {
-                "_source": source,
-                "size": 6,
-
-                "query": {
-                    "function_score" : {
-                        "query" : {
-                            "match_phrase" : {
-                                "str" : query.trim()
-                            }
-                        },
-
-                        "functions" : [
-                            // Prefer SnomedCT / MeSH
-                            {
-                                "filter": {
-                                    "terms": { "source": ["SNOMEDCT_US", "MSH", "MSHDUT"] }
-                                },
-                                "weight": 1.25
-                            },
-
-                            // Negative weight for some categories
-                            {
-                                "filter": {
-                                    "terms": { "types": ["Health Care Activity", "Biomedical Occupation or Discipline"] }
-                                },
-                                "weight": 0.7
-                            }
-                        ]
-                    }
-                }
-            };
-        }
-
         var queryObj = {
             "index" : 'autocomplete',
             "body"  : elastic_query
