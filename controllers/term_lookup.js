7e70b461c0d62c7f6e8413d9c2fac1b53be11821
Fabiën <fabientesselaar@gmail.com>
2016-02-09 14:31:57 +0100

    Added fuzziness for term_lookup

--- controllers/term_lookup.js
+++ controllers/term_lookup.js
@@ -12,16 +12,14 @@ var elasticClient = new elastic.Client({
 ***REMOVED***);
 
 
-const source = ["cui", "str", "exact", "pref", "source", "types"];
+const source = ["cui", "str", "exact", "pref"];
 
 
 module.exports = function *() {
   var query = this.request.body.query;
 
-
   // Lookup matches in Elasticsearch
   var exactMatches = yield findExact(query);
-  // var closeMatches = yield findMatches(query);
 
   this.body = {
     "took": exactMatches.took,
@@ -41,91 +39,16 @@ function findExact(query) {
             "size": 3,
 
             "query": {
-                "term" : {
-                    "exact" : wantedTerm
+                "fuzzy" : {
+                    "exact" : {
+                        "value": wantedTerm,
+                        "fuzziness": (wantedTerm.length > 5 ? 2 : 0)
+                ***REMOVED***
             ***REMOVED***
         ***REMOVED***
     ***REMOVED***;
 
     ***REMOVED*** Search in all indexes
-        var queryObj = {
-            "index" : 'autocomplete',
-            "body"  : elastic_query
-    ***REMOVED***;
-
-        elasticClient.search(queryObj, function(err, res) {
-            var hits = res.hits;
-            var result = [];
-
-            if (hits && hits.total > 0) {
-                for (var i=0; i < hits.hits.length; i++) {
-                    result.push(hits.hits[i]._source);
-            ***REMOVED***
-        ***REMOVED***
-
-            callback(err, {
-                "took": res.took,
-                "hits": result
-        ***REMOVED***);
-    ***REMOVED***);
-***REMOVED***
-***REMOVED***
-
-
-function findMatches(query) {
-    var origin = guess_origin(query);
-
-***REMOVED*** Filter out CUI codes that the user already selected
-    return function(callback) {
-
-    ***REMOVED*** DBC code check needs prefix matching
-        if (origin === "code") {
-            var elastic_query =  {
-                "_source": source,
-                "size": 6,
-
-                "query": {
-                    "match_phrase_prefix" : {
-                        "str" : query.trim()
-                ***REMOVED***
-            ***REMOVED***
-        ***REMOVED***;
-    ***REMOVED***
-        ***REMOVED***
-            var elastic_query =  {
-                "_source": source,
-                "size": 6,
-
-                "query": {
-                    "function_score" : {
-                        "query" : {
-                            "match_phrase" : {
-                                "str" : query.trim()
-                        ***REMOVED***
-                    ***REMOVED***,
-
-                        "functions" : [
-                        ***REMOVED*** Prefer SnomedCT / MeSH
-                            {
-                                "filter": {
-                                    "terms": { "source": ["SNOMEDCT_US", "MSH", "MSHDUT"] ***REMOVED***
-                            ***REMOVED***,
-                                "weight": 1.25
-                        ***REMOVED***,
-
-                        ***REMOVED*** Negative weight for some categories
-                            {
-                                "filter": {
-                                    "terms": { "types": ["Health Care Activity", "Biomedical Occupation or Discipline"] ***REMOVED***
-                            ***REMOVED***,
-                                "weight": 0.7
-                        ***REMOVED***
-                        ]
-                ***REMOVED***
-            ***REMOVED***
-        ***REMOVED***;
-    ***REMOVED***
-
         var queryObj = {
             "index" : 'autocomplete',
             "body"  : elastic_query
