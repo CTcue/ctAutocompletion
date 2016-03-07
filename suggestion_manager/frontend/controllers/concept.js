57bbbbdb5fcadeb8f197574b470c66483d93f060
Fabiën <fabientesselaar@gmail.com>
2016-03-07 17:31:25 +0100

    Allow custom term to be edited/added

--- suggestion_manager/frontend/controllers/concept.js
+++ suggestion_manager/frontend/controllers/concept.js
@@ -2,7 +2,10 @@
 
 app.controller('conceptController', function ($scope, UMLS, CRUD) {
 
+    $scope.error    = false;
+    $scope.active_term = false;
     $scope.concepts = [];
+
 
     CRUD.list(UMLS.url, "umls")
         .then(function(result) {
@@ -15,4 +18,24 @@ app.controller('conceptController', function ($scope, UMLS, CRUD) {
     //             List.remove($scope.addedTerms, item)
     //         });
     // }
+
+    $scope.setActive = function(item) {
+        $scope.active_term = item;
+    }
+
+    $scope.submit = function(active_term) {
+        console.log(active_term);
+
+        // CRUD.create(UMLS.url, 'umls', item)
+        //   .then(function(result) {
+        //       $scope.success = true;
+        //       $scope.error   = false;
+
+        //       $scope.item = {}
+        //   },
+        //   function(err) {
+        //       $scope.error = err;
+        //   });
+    }
+
 });
