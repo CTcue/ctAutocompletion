967c104721c0ece05d0e0852a3d3500061cf852c
Fabien Tesselaar <fabientesselaar@gmail.com>
2016-04-25 19:43:14 +0200

    Updated preprocessing to include LAT + PREF term

--- _scripts/preprocess/main.py
+++ _scripts/preprocess/main.py
@@ -3,6 +3,8 @@
 
 from __future__ import unicode_literals
 from mrjob.job import MRJob
+from collections import defaultdict
+
 
 skip_categories = [
     "Age Group",
@@ -27,12 +29,19 @@ skip_categories = [
 
 class AggregatorJob(MRJob):
     """
-    Groups unique synonyms by CUI.
-    Output format: (CUI, [STR, STR, ...])
+    Groups unique terms/synonyms by CUI, for each language.
+    Output format: (CUI, LANG, PREF_TERM, [TYPES], [STR, STR, ...])
     """
 
     def mapper(self, key, value):
         split = value.decode("utf-8").split("|")
+
+
+        # TODO
+        # - Parse string for 'types'
+        # - Normalize strings (Lowercase, unless short + full uppercase)
+
+
 
         # MRCONSO Header
         if len(split) == 19:
@@ -59,42 +68,63 @@ class AggregatorJob(MRJob):
             if STR.count(".") >= 3 or STR.count(":") >= 3:
                 return
 
-            yield CUI, ["A", STR]
+            if TS == "P":
+                yield CUI, ["PREF", LAT, STR]
+            else:
+                yield CUI, ["TERM", LAT, STR]
+
 
         # Additional Terms header
         elif len(split) == 4:
             (CUI, STR, LAT, SAB) = split
 
-            yield CUI, ["A", STR]
+            if LAT == "nl":
+                LAT = "DUT"
+            elif LAT == "en":
+                LAT = "ENG"
+
+            yield CUI, ["PREF", LAT, STR]
 
         # MRSTY Header
         elif len(split) == 7:
             (CUI, TUI, STN, STY, ATUI, CVF, _) = split
 
-            yield CUI, ["B", STY]
+            yield CUI, ["STY", STY]
         else:
             pass
 
 
     def reducer(self, key, values):
-        terms = set()
+        terms = defaultdict(set)
+        preferred = defaultdict(list)
+
         types = set()
 
         for value in values:
-            if value[0] == "A":
-                terms.add(value[1])
-            elif value[0] == "B":
+            if value[0] == "TERM":
+                terms[value[1]].add(value[2])
+
+            if value[0] == "PREF":
+                preferred[value[1]].append(value[2])
+                terms[value[1]].add(value[2])
+
+            elif value[0] == "STY":
                 types.add(value[1])
 
+
+        # Check the types
         if not terms or not types:
             return
 
-        # Check types
         if any((x for x in types if x in skip_categories)):
             return
 
-        out = "%s\t%s\t%s" % (key, "|".join(types), "|".join(terms))
-        print out.encode("utf-8")
+
+        for LAT, v in terms.iteritems():
+            if LAT in preferred:
+                out = "%s\t%s\t%s\t%s\t%s" % (key, LAT, preferred[LAT][0], "|".join(types), "|".join(v))
+                print out.encode("utf-8")
+
 
 
 if __name__ == "__main__":
