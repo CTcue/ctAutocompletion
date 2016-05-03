a7db32f12396b462690ddcdf99c6dc34bc08fb35
Fabien Tesselaar <fabientesselaar@gmail.com>
2016-05-03 13:46:45 +0200

    Updated semantic type to parent category

---
+++ _scripts/preprocess/semantic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import unicodecsv as csv
+import os
+
+basepath = os.path.dirname(__file__)
+
+semantic_types = {***REMOVED***
+with open(os.path.join(basepath, 'semantic_types.txt')) as t:
+    datareader = csv.reader(t, encoding="utf-8", delimiter=str("|"))
+    for line in datareader:
+        (abbr, group, code, description) = line
+        semantic_types[code] = abbr
+
+
+def get_group(code):
+    if code in semantic_types:
+        return semantic_types[code]
+
+    return ""
---
+++ _scripts/preprocess/testie.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from normalize import normalize
+
+
+if __name__ == "__main__":
+
+    print normalize("Absorptiometrie, foton-")
+    print normalize("Adhesive Tape - Tape")
+    print normalize("Complex, aidsdementie- (ADC)")
+    print normalize("Addison's disease NOS")
