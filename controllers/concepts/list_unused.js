44330b1f223ee6a9321042dd029c6cbb0e48a081
Fabien Tesselaar <fabientesselaar@gmail.com>
2016-08-30 12:22:31 +0200

    Small update in listing unused added-terms script

--- controllers/concepts/list_unused.js
+++ controllers/concepts/list_unused.js
@@ -1,20 +1,27 @@
 "use strict";
 
+
+/////
+// Temp script
+// - could be converted into something for admins
+
+
 var db    = require('../../lib/database');
 var table = db.table('umls');
 
 
 var findUnused = {
     "$or": [
+      { 'user._id': "57b5c839d0d42b53502a0d0e" ***REMOVED***,
       { 'user.name': "Admin powers" ***REMOVED***,
-      { 'user.name': "Normal user" ***REMOVED***,
       { 'user.env' : "TEST_ENVIRONMENT" ***REMOVED***
     ]
 ***REMOVED***;
 
+// find -- remove
 var list = table.find(findUnused, function(err, list) {
 
     console.log(list)
-
     process.exit(0);
+
 ***REMOVED***);
